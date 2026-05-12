c4eabf1897badd7378eb478ba3aa448d1f443b4e
minhtv901 <le5875970@gmail.com>
2026-05-12 14:01:05 +0700

    Fix static GitHub Pages deploy

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+import "./styles.css";
+
+async function main() {
+  const router = await getRouter();
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  );
+}
+
+main();
