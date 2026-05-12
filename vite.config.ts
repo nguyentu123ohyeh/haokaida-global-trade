2ac960549ff87ba49b99fc95484fba41ed255b12
minhtv901 <le5875970@gmail.com>
2026-05-12 13:27:23 +0700

    ádasd

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   // Mấy cái plugin như react() đã có sẵn bên trong rồi, đại ca không cần add lại đâu
   vite: {
-    base: '/haokaida-global-trade/', // Chèn base vào trong mục vite này mới chuẩn
+    base: '/', // Vì dùng domain riêng nên để là root luôn đại ca ạ
   },
   tanstackStart: {
     server: { entry: "server" },
